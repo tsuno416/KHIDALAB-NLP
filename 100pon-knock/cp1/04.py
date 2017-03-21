a8669b32ebd7b4b1df5c12a91ffd0db045c69262
eyoitsu <yoichiro.tsunoda@ericsson.com>
2017-03-21 17:23:31 +0900

    update knock

--- 100pon-knock/cp1/04.py
+++ 100pon-knock/cp1/04.py
@@ -5,19 +5,28 @@
 import sys
 import re
 
-def count_words(string):
+def pick_up_words(string):
     result_list = []
+    result_hash = {}
     for word  in re.split('\W+',string):
-        result_list.append(len(word))
-    return result_list
+        result_list.append(word)
+    for word in result_list:
+        if result_list.index(word) + 1  in[1,5,6,7,8,9,15,16,19]:
+            kigou = word[:1]
+        else: 
+            kigou = word[:2] 
+        result_hash[kigou] = result_list.index(word) + 1 
+        
+    return result_hash
 
 if __name__ == "__main__":
     '''
-    print length of words  from  given string
+    print specific char of the words  from  given string
     this only support 1 arguments
-    original param should be "Now I need a drink, alcoholic of course,\
-     after the heavy lectures involving quantum mechanics."
+    original param should be "Hi He Lied Because Boron\
+    Could Not Oxidize Fluorine. New Nations Might Also\
+    Sign Peace Security Clause. Arthur King Can."
     '''
     string = sys.argv[1]
-    result_list = count_words(string)
-    print(result_list)
+    result_hash = pick_up_words(string)
+    print(result_hash)
